6cf66c3906b232af62107d47a891a66a9362da2e
itstheonlychris <cz.chrisfilms@gmail.com>
2020-01-02 20:56:25 -0600

    Change notification function to use Slack instead of SendGrid Emails

--- src/components/FormQuestions.js
+++ src/components/FormQuestions.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import RadioGroup from './Form/RadioGroup';
 
-const URL = '/.netlify/functions/SendEmail';
+const URL = '/.netlify/functions/SendSlack';
 
 export class FormQuestions extends Component {
 	state = {
